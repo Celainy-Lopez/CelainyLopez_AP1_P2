c8717679ae37ca7aa324ef61272a6c6729d63f6c
celainy-lopez <celainy_lopez@ucne.edu.do>
2024-11-14 20:30:45 -0400

    Agregada plantilla del servicio

---
+++ Services/RegistrosService.cs
@@ -0,0 +1,60 @@
+﻿using CelainyLopez_AP1_P2.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CelainyLopez_AP1_P2.Services;
+
+public class RegistrosService(IDbContextFactory<Context> DbFactory)
+{
+    /*
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+
+    private async Task<bool> Insertar(Registros registros)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+
+    private async Task<bool> Modificar(Registros registros)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+
+    public async Task<List<Registros>> Listar(Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+
+    }
+
+    public async Task<Registros> Buscar(int id)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+
+    public async Task<Registros> BuscarDetalles(int id)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+
+    public async Task<bool> Guardar(Registros registros)
+    {
+        await using var contexto = await DbFactory
+            .CreateDbContextAsync();
+    }
+    */
+}
